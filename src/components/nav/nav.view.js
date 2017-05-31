ed3d7b5515a2f4dabfb216a0f777f80efb8c998e
JaeYeopHan <ljyhanll@gmail.com>
2017-05-31 22:51:54 +0900

    arrange code nav.view.js

--- src/components/nav/nav.view.js
+++ src/components/nav/nav.view.js
@@ -9,6 +9,7 @@ function NavView(eventEmitter, root, navOption) {
     this._init();
 }
 
+// index에 따른 page render
 NavView.prototype.onClickPageNav = function(e) {
     e.preventDefault();
     var index = $(e.target).text();
@@ -18,10 +19,10 @@ NavView.prototype.onClickPageNav = function(e) {
         max: this._MAX_TODO_COUNT_OF_PAGE
     };
     this.eventEmitter.emit('changePage', renderOption);
-    this.navSelected();
-    this.prevCheck();
+    this.controlNav();
 };
 
+// prev button에 따른 페이지 이동
 NavView.prototype.onClickPrevBtn = function(e) {
     e.preventDefault();
     if ($(e.target).closest('li').hasClass('disabled')) {
@@ -33,38 +34,36 @@ NavView.prototype.onClickPrevBtn = function(e) {
         max: this._MAX_TODO_COUNT_OF_PAGE
     };
     this.eventEmitter.emit('changePage', renderOption);
-    this.navSelected();
-    this.prevCheck();
+    this.controlNav();
 };
 
+// post button에 따른 페이지 이동
 NavView.prototype.onClickPostBtn = function(e) {
     e.preventDefault();
-    this._currentIndex += 1;
-    if (this._currentIndex > 5) {
-        if ($(e.target).closest('li').hasClass('disabled')) {
-            return;
-        }
-        // TODO
-        // controller에서 새로운 이벤트 등록
-        // renderNav 다시. _currentIndex + 1 부터 되는대로.
+    if ($(e.target).closest('li').hasClass('disabled')) {
+        return;
     }
+
+    // TODO
+    // controller에서 새로운 이벤트 등록
+    // renderNav 다시. _currentIndex + 1 부터 되는대로.
+
+    this._currentIndex += 1;
     var renderOption = {
         index: this._currentIndex,
         max: this._MAX_TODO_COUNT_OF_PAGE
     };
     this.eventEmitter.emit('changePage', renderOption);
-    this.navSelected();
-    this.prevCheck();
+    this.controlNav();
 };
 
 NavView.prototype.renderNav = function(renderOption) {
     var renderOption = renderOption || [{ num: 1, post: false }];
-    this.post = renderOption.post;
+    this.isOverIndex = renderOption.post;
     $(this.root).html(navTemplate({
         pages: renderOption.pages
     }));
-    this.navSelected();
-    this.prevCheck();
+    this.controlNav();
 };
 
 NavView.prototype.navSelected = function() {
@@ -79,10 +78,24 @@ NavView.prototype.navSelected = function() {
 
 NavView.prototype.prevCheck = function() {
     var prev = $('#prevBtn').parent();
-    this._currentIndex == 1
+    this._currentIndex === 1
         ? prev.addClass('disabled')
         : prev.removeClass('disabled');
 };
+
+NavView.prototype.postCheck = function() {
+    var post = $('#postBtn').parent();
+    //TODO Add logic
+    this._currentIndex === 5 && this.isOverIndex === false
+        ? post.addClass('disabled')
+        : post.removeClass('disabled');
+};
+
+NavView.prototype.controlNav = function() {
+    this.navSelected();
+    this.prevCheck();
+    this.postCheck();
+}
 
 NavView.prototype._init = function() {
     $(this.root).on('click', function(e) {
