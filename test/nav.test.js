9ece4f97509ddd9180481caca6345320929fd97d
JaeYeopHan <ljyhanll@gmail.com>
2017-06-02 16:27:15 +0900

    Ready to test

---
+++ test/nav.test.js
@@ -0,0 +1,12 @@
+var NavService = require('../src/components/nav/nav.service');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('test', function() {
+    it('test for test', function(done) {
+        expect("").to.equal("");
+        done();
+    });
+});
