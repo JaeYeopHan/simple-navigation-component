04ef614a3decd0871380c918470630c05b8cda72
JaeYeopHan <ljyhanll@gmail.com>
2017-06-16 14:37:49 +0900

    Remove jquery in list.view.js

--- src/components/list/list.view.js
+++ src/components/list/list.view.js
@@ -1,14 +1,13 @@
-import $ from 'jquery';
 import listTemplate from './list.hbs';
 
 function ListView(eventEmitter, root) {
-    this.root = root;
+    this.root = document.querySelector(root);
     this._eventEmitter = eventEmitter;
 }
 
 ListView.prototype.renderList = function(todos) {
     var todos = todos || { id: 1, todo: 'todos가 없습니다.' };
-    $(this.root).html(listTemplate({ todos: todos }));
+    this.root.innerHTML = listTemplate({ todos: todos });
 };
 
 module.exports = ListView;
